ab7318041e1112d0c349cc72a3054ce27afc62b2
Jack Richards <jsrichards@hey.com>
2023-11-09 12:15:28 +0000

    Sends some CSV data as a response when route /api/bar-chart is hit. Progress towards user story #6.

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+import polars as pl
 
 app = FastAPI()
-
-# Test comment.
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/api/bar-chart")
+async def get_bar():
+    thermal_characteristics_after_ee_json = pl.read_csv(
+        '../../Quantification of inherent flexibility from electrified residential heat sector in England and Wales/\
+        01 - Thermal_Characteristics/\
+        Thermal_characteristics_afterEE.csv').to_json()
+    
+    thermal_characteristics_before_ee_json = pl.read_csv(
+        '../../Quantification of inherent flexibility from electrified residential heat sector in England and Wales/\
+        01 - Thermal_Characteristics/\
+        Thermal_characteristics_beforeEE.csv').to_json()
+    
+    bar_chart_json = {
+        "thermal_characteristics_after_ee": thermal_characteristics_after_ee_json,
+        "thermal_characteristics_before_ee": thermal_characteristics_before_ee_json
+    }
+    
+    return bar_chart_json
