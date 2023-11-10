f28378bdf41629c44fa95ad30352cd5c23861ab9
c21052928 <richardsj31@cardiff.ac.uk>
2023-11-10 15:01:41 +0000

    Reads large CSV in batches. 15 rows at a time. Concats the 15 rows then converts them into ipc Arrow format.
    We then store the data in redis with a unique key.
    
    Is able to handle processing a CSV with 1 million + rows

--- initialise_csv.py
+++ initialise_csv.py
@@ -8,19 +8,22 @@ csv_files = glob.glob(directory + "/*.csv")
 
 import polars as pl
 
-# from utils.redis_pool import get_redis
+from utils.redis_pool import get_redis
 
-# r = get_redis()
+r = get_redis()
 
 # Calculate the number of rows in the csv file.
 # This is used to calculate the number of chunks.
+
 for file in csv_files:
-    # Create reader to read CSV in batches.
+    batch_count = 1
     reader = pl.read_csv_batched(file, batch_size=100)
     # Process 15 rows at a time.
     while True:
         batches = reader.next_batches(15)
         if batches is None:
             break
-        for df in batches:
-            print(df)
+        concat_batch = pl.concat(batches)
+        ipc_batch = concat_batch.write_ipc_stream()
+        r.set(f"caches:dataframes:{file}:{batch_count}:original", ipc_batch)
+        batch_count = batch_count + 1
