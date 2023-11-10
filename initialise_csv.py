69896e3aba19def93abf980465f6bad22258a941
Jack Richards <richardsj31@cardiff.ac.uk>
2023-11-10 17:48:45 +0000

    Changed the way we load the file to load it all into memory at once. Better for reasons discussed in codes comments.
    User story #10 is completed by this commit.

--- initialise_csv.py
+++ initialise_csv.py
@@ -1,4 +1,5 @@
 import glob
+import asyncio
 
 # specify the directory
 directory = "data"
@@ -12,18 +13,33 @@ from utils.redis_pool import get_redis
 
 r = get_redis()
 
-# Calculate the number of rows in the csv file.
-# This is used to calculate the number of chunks.
+# Iterates over all CSV files, reading them into a DataFrame
+# and storing the IPC stream to Redis with a unique key.
+async def initialise_csv():
+    for file in csv_files:
+        # Store file name without .csv at end and without the directory 'data\' at the beginning.
+        file_name = file[len(directory)+1:-4]
 
-for file in csv_files:
-    batch_count = 1
-    reader = pl.read_csv_batched(file, batch_size=100)
-    # Process 15 rows at a time.
-    while True:
-        batches = reader.next_batches(15)
-        if batches is None:
-            break
-        concat_batch = pl.concat(batches)
-        ipc_batch = concat_batch.write_ipc_stream(None)
-        r.set(f"caches:dataframes:{file}:{batch_count}:original", ipc_batch)
-        batch_count = batch_count + 1
+        # More inefficient than my last approach in terms of reading the file. However, after discussing
+        # the issue with Kavin we agreed that this method is the best in this scenario.
+        # Since it allows us to store the entire set of data as a value in Redis. Instead of storing batches
+        # of rows with a different key for each batch.
+
+        # Additionally, this initalisation code is typically ran once a year, so low efficiency isn't a huge problem here.
+        reader = pl.read_csv(file)
+        ipc = reader.write_ipc_stream(None).getvalue()
+        try:
+            await r.set(f"caches:dataframes:{file_name}:original", ipc)
+        except Exception as e:
+            print(e)
+        print(f"| Success |: Stored {file_name}'s contents in Redis.")
+
+    # Test reading from Redis. Works as intended.
+    # data = await r.get("caches:dataframes:Thermal_characteristics_afterEE:original")
+    # polars_data = pl.read_ipc_stream(data)
+    # print(polars_data)
+async def main():
+    await initialise_csv()
+
+if __name__ == "__main__":
+    asyncio.run(main())
