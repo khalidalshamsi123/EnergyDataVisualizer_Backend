8a1119294d575eb8ea536ddb15af28ee5f216f13
c21086065 <alshamsik@cardiff.ac.uk>
2023-11-28 14:24:56 +0000

    merge conflicts resolved

--- main.py
+++ main.py
@@ -93,3 +93,11 @@ async def get_pie():
         final_averages.append([tuple[0], final_mean])  # Name of heating type. Oil, gas etc. And the mean.
 
     return final_averages
+
+#Line Graph API reader
+@app.get("/api/line-graph")
+async def get_line_graph():
+    #reading the csv file 'Half-Hourly-profiles-ofheating-technologies'
+    json_line_graph = pl.read_csv("C:\\Users\\c21086065\\OneDrive - Cardiff University\\Y3-Commercial Frameworks\\dataset\\Spatio-temporal heat demand for LSOAs in England and Wales\\Half-hourly_profiles_of_heating_technologies.csv")
+    #creating a dictionary 
+    return json_line_graph.to_dicts()
