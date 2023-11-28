08edf4695ce176b767f7d6e7b6bd2c2ef16b6a2e
c21086065 <alshamsik@cardiff.ac.uk>
2023-11-28 14:02:16 +0000

    added a comment

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def root():
 
 @app.get("/api/line-graph")
 async def get_line_graph():
-    #reading the csv file
+    #reading the csv file 'Half-Hourly-profiles-ofheating-technologies'
     json_line_graph = pl.read_csv("C:\\Users\\c21086065\\OneDrive - Cardiff University\\Y3-Commercial Frameworks\\dataset\\Spatio-temporal heat demand for LSOAs in England and Wales\\Half-hourly_profiles_of_heating_technologies.csv")
     #creating a dictionary 
     return json_line_graph.to_dicts()
