d069b81ac5c814809d956c199e2b9fb00e67db24
Jack Richards <richardsj31@cardiff.ac.uk>
2023-12-03 21:37:44 +0000

    Progress towards https://git.cardiff.ac.uk/c21052928/energy-data-centre-visualisation/-/issues/16.
    Refactored the routes for the Annual_heat_demand_LSOA into one single route. Much cleaner and pairs well with the new frontend code to easily generate multiple graphs from the data retrieved by one request.

--- routes/Annual_heat_demand_LSOA.py
+++ routes/Annual_heat_demand_LSOA.py
@@ -2,11 +2,18 @@ import polars as pl
 
 from fastapi import APIRouter
 
+from pydantic import BaseModel
+
 from utils.methods import get_averages_for_columns_including_word_regex, get_averages_for_columns_including_word, get_sum_for_columns_including_word_regex, get_sum_for_columns_including_word
+
+# Create a class for the options object.
+class Option_Object(BaseModel):
+    filter: str | None = None
+    rows: str | None = None
 
 router = APIRouter()
 
-csv_column_names = [
+before_csv_column_names = [
     "Average heat demand before energy efficiency measures for detached biomass boiler (kWh)",
     "Average heat demand before energy efficiency measures for detached gas boiler (kWh)",
     "Average heat demand before energy efficiency measures for detached oil boiler (kWh)",
@@ -25,18 +32,77 @@ csv_column_names = [
     "Average heat demand before energy efficiency measures for terraced resistance heating (kWh)"
 ]
 
-@router.get("/api/Annual_heat_demand_LSOA/heating_type/before")
-async def get_average_heat_demand_per_heating_type():
-    column_names = ["gas boiler", "oil boiler", "resistance heating", "biomass boiler"]
-    average_heat_demand_per_heating_type = await get_averages_for_columns_including_word_regex(column_names, csv_column_names, 'Annual_heat_demand_LSOA')
-    sum_heat_demand_per_heating_type = await get_sum_for_columns_including_word_regex(column_names, csv_column_names, 'Annual_heat_demand_LSOA')
-    return  { "average": average_heat_demand_per_heating_type, "sum": sum_heat_demand_per_heating_type }
+after_csv_column_names = [
+    "Average heat demand after energy efficiency measures for detached biomass boiler (kWh)",
+    "Average heat demand after energy efficiency measures for detached gas boiler (kWh)",
+    "Average heat demand after energy efficiency measures for detached oil boiler (kWh)",
+    "Average heat demand after energy efficiency measures for detached resistance heating (kWh)",
+    "Average heat demand after energy efficiency measures for flat biomass boiler (kWh)",
+    "Average heat demand after energy efficiency measures for flat gas boiler (kWh)",
+    "Average heat demand after energy efficiency measures for flat oil boiler (kWh)",
+    "Average heat demand after energy efficiency measures for flat resistance heating (kWh)",
+    "Average heat demand after energy efficiency measures for semi-detached biomass boiler (kWh)",
+    "Average heat demand after energy efficiency measures for semi-detached gas boiler (kWh)",
+    "Average heat demand after energy efficiency measures for semi-detached oil boiler (kWh)",
+    "Average heat demand after energy efficiency measures for semi-detached resistance heating (kWh)",
+    "Average heat demand after energy efficiency measures for terraced biomass boiler (kWh)",
+    "Average heat demand after energy efficiency measures for terraced gas boiler (kWh)",
+    "Average heat demand after energy efficiency measures for terraced oil boiler (kWh)",
+    "Average heat demand after energy efficiency measures for terraced resistance heating (kWh)"
+]
 
-@router.get("/api/Annual_heat_demand_LSOA/dwelling_type/before")
-async def get_average_heat_demand_per_dwelling_type():
-    column_names = ["detached", "flat", "semi-detached", "terraced"]
-    average_heat_demand_per_dwelling_type = await get_averages_for_columns_including_word(column_names, csv_column_names, 'Annual_heat_demand_LSOA')
-    sum_heat_demand_per_dwelling_type = await get_sum_for_columns_including_word(column_names, csv_column_names, 'Annual_heat_demand_LSOA')
-    return { "average": average_heat_demand_per_dwelling_type, "sum": sum_heat_demand_per_dwelling_type }
+def create_data_object():
+    data = {}
+    data['average'] = {}
+    data['sum'] = {}
+    return data
 
-# Still need the 'after' routes.
+@router.post("/api/Annual_heat_demand_LSOA")
+async def get_requested_data_for_charts(options_object: list[Option_Object]):
+    dwelling_type_columns = ["detached", "flat", "semi-detached", "terraced"]
+    heating_type_columns = ["gas boiler", "oil boiler", "resistance heating", "biomass boiler"]
+
+    datasets = {}
+    # Loop through the options object. Create a new object for each filter and rows containing the average and sum.
+    for object in options_object:
+        if object.filter == "heating_type" and object.rows == "before":
+            # Create a unique key based on the filter and rows.
+            key = f'{object.filter}:{object.rows}'
+            # Create a object containing the keys average and sum.
+            data = create_data_object()
+            data['average'] = await get_averages_for_columns_including_word_regex(heating_type_columns, before_csv_column_names, 'Annual_heat_demand_LSOA')
+            data['sum'] = await get_sum_for_columns_including_word_regex(heating_type_columns, before_csv_column_names, 'Annual_heat_demand_LSOA')
+            # Add the key alongside it's data to the overall datasets object.
+            datasets[key] = data
+        elif object.filter == "heating_type" and object.rows == "after":
+            # Create a unique key based on the filter and rows.
+            key = f'{object.filter}:{object.rows}'
+            # Create a object containing the keys average and sum.
+            data = create_data_object()
+            data['average'] = await get_averages_for_columns_including_word_regex(heating_type_columns, after_csv_column_names, 'Annual_heat_demand_LSOA')
+            data['sum'] = await get_sum_for_columns_including_word_regex(heating_type_columns, after_csv_column_names, 'Annual_heat_demand_LSOA')
+            # Add the key alongside it's data to the overall datasets object.
+            datasets[key] = data
+        elif object.filter == "dwelling_type" and object.rows == "before":
+            # Create a unique key based on the filter and rows.
+            key = f'{object.filter}:{object.rows}'
+            # Create a object containing the keys average and sum.
+            data = create_data_object()
+            data['average'] = await get_averages_for_columns_including_word(dwelling_type_columns, before_csv_column_names, 'Annual_heat_demand_LSOA')
+            data['sum'] = await get_sum_for_columns_including_word(dwelling_type_columns, before_csv_column_names, 'Annual_heat_demand_LSOA')
+            # Add the key alongside it's data to the overall datasets object.
+            datasets[key] = data
+        elif object.filter == "dwelling_type" and object.rows == "after":
+            # Create a unique key based on the filter and rows.
+            key = f'{object.filter}:{object.rows}'
+            # Create a object containing the keys average and sum.
+            data = create_data_object()
+            data['average'] = await get_averages_for_columns_including_word(dwelling_type_columns, after_csv_column_names, 'Annual_heat_demand_LSOA')
+            data['sum'] = await get_sum_for_columns_including_word(dwelling_type_columns, after_csv_column_names, 'Annual_heat_demand_LSOA')
+            # Add the key alongside it's data to the overall datasets object.
+            datasets[key] = data
+
+    print(datasets)
+
+    # Return the datasets array.
+    return datasets
