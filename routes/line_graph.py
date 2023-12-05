0b6ecb00ba543b0a3dc5113cd66bae8bb5d0710f
Kavin Sundar <sundark@cardiff.ac.uk>
2023-12-05 13:23:10 +0000

    Optimize and change the data structure of the line graph response
    - no longer return a dictionary of field name: value
    - date is converted to unix epoch in second
    - have an array called fields for all the columns
    - have an array in the dictionary for each field name
    - convert values to GWh and round them to 4 decimal precission
    - calculate min max for all values
    
    Co-authored-by: c21086065 <alshamsik@cardiff.ac.uk>

--- routes/line_graph.py
+++ routes/line_graph.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter, Response
+import copy
+import math
+
+import polars as pl
+from fastapi import APIRouter
 
 from utils.dataset_utils import get_csv_frame
 
@@ -6,9 +10,38 @@ router = APIRouter()
 
 
 @router.get("/api/line-graph")
-async def get_line_graph():
+async def get_line_graph(fields: str):
+    fields = fields.split(",")
 
-    df = await get_csv_frame("Half-hourly_profiles_of_heating_technologies")
+    df = await get_csv_frame("Half-hourly_profiles_of_heating_technologies", columns=fields)
 
-    # creating a dictionary
-    return Response(df.write_json(row_oriented=True), media_type="application/json")
+    # parse index column as datetime
+    df = df.with_columns(
+        pl.col("index").str.to_datetime(format="%Y-%m-%dT%H:%M:%S").cast(pl.Int64).apply(lambda x: int(x / 1000000)))
+
+    min_val = math.inf
+    max_val = -math.inf
+
+    for column in df.columns:
+        if column != "index":
+            if df[column].dtype == pl.Float64:
+                df = df.with_columns(pl.col(column).apply(lambda x: round(x * 1000000, 4)))
+
+                series_min = df[column].min()
+                series_max = df[column].max()
+
+                min_val = series_min if series_min < min_val else min_val
+                max_val = series_max if series_max > max_val else max_val
+
+    data = {}
+
+    columns = copy.deepcopy(df.columns)
+
+    data["columns"] = fields
+
+    data["minmax"] = [min_val, max_val]
+
+    for column in columns:
+        data[column] = df[column].to_list()
+
+    return data
