cc63c324f064faabfaebaffac5ad18c5a01cd836
Kavin Sundar <sundark@cardiff.ac.uk>
2023-12-20 16:25:07 +0000

    Allow using any dataset and csv for tables api.

--- routes/tables.py
+++ routes/tables.py
@@ -1,25 +1,20 @@
-import os
-
-from fastapi import APIRouter, Response
 import polars as pl
+from fastapi import APIRouter, Response
+
+from utils.dataset_utils import get_csv_frame
 
 router = APIRouter()
 
-dataset = os.getenv("DATA_DIR")
-
-paths = {
-    "ukerc_scotland_data": f"{dataset}/Residential heat demand in LSOAs in Scotland/ukerc_scotland_data.csv",
-}
-
 
 @router.get("/tables/{table_id}")
-async def read_table(table_id: str, fields: str):
-    if table_id not in paths:
+async def read_table(table_id: str, fields: str, dp: int = 2):
+    df = await get_csv_frame(table_id, fields.split(","))
+
+    if df is None:
         return {"message": f"Table {table_id} not found."}
 
-    df = pl.read_csv(paths[table_id])
-
-    fields = fields.split(",")
-    df = df.select(fields)
+    for column in df.columns:
+        if df[column].dtype == pl.Float64:
+            df = df.with_columns(pl.col(column).apply(lambda x: round(x, dp)))
 
     return Response(df.write_json(row_oriented=True), media_type="application/json")
